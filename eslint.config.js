94efbed5480be305ceb08ded84a1e2c51a704682
Kashyap Patel <kashyap.jsdev@gmail.com>
2026-03-02 14:50:53 +0530

    added the husky and configed

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import js from '@eslint/js'
 import globals from 'globals'
+import importPlugin from 'eslint-plugin-import'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default [
   js.configs.recommended,
   {
+    plugins: { import: importPlugin, 'unused-imports': unusedImports },
     ignores: ['dist/**', 'node_modules/**'],
     languageOptions: {
       ecmaVersion: 2022,
@@ -13,8 +17,18 @@ export default [
       }
     },
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-      'no-console': 'off'
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+      'import/no-unresolved': 'error',
+      'import/no-extraneous-dependencies': [
+        'error',
+        { devDependencies: ['**/eslint.config.js', '**/*.test.js'] }
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+      ]
     }
   }
 ]
